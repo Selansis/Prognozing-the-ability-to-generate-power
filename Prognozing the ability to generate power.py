23823bce88858baf2779346a7c8cddf0f46c2d06
Selansis <wojtekmichalik24@gmail.com>
2024-04-21 12:20:33 +0200

    neural link etc

--- Prognozing the ability to generate power.py
+++ Prognozing the ability to generate power.py
@@ -1,9 +1,51 @@
+def filtering(data,input_regex):
+  return data.filter(regex='Date|'+ input_regex)
+
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPRegressor
 import csv 
+import matplotlib.pyplot
 import pandas as pd 
-capacity = pd.read_csv('Energy Poland Installed capacity.csv')
-mixEnergy = pd.read_csv('Mix Energy Poland.csv')
+import matplotlib.pyplot as plt
+capacity = pd.read_csv('Energy Poland Installed capacity.csv', usecols=['Date', 'Installed capacity solar Poland', 'Installed capacity energy_storage Poland'])
+mixEnergy = pd.read_csv('Mix Energy Poland.csv', usecols =['Date','Aggregated Generation Per Type, PSE SA CA, Actual Generation Output, Solar'])
 hourMeteo = pd.read_csv('Godzinowe dane meteo dla poszczególnych lokalizacji.csv')
-before_cloud = pd.read_csv('przed_uwzg_zachmurzenia.csv')
+before_cloud = pd.read_csv('przed_uwzg_zachmurzenia.csv', encoding='utf-8')
 after_cloud = pd.read_csv('po_uwzg_zachmurzenia.csv')
 
-print(capacity)
+capacity = pd.DataFrame(capacity)
+mixEnergy = pd.DataFrame(mixEnergy)
+hourMeteo = pd.DataFrame(hourMeteo)
+before_cloud = pd.DataFrame(before_cloud)
+after_cloud = pd.DataFrame(after_cloud)
+
+'''
+krakow_before = filtering(before_cloud,"Krak")
+warszawa_before = filtering(before_cloud,"Warszawa")
+wroclaw_before = filtering(before_cloud,"Wroc")
+gdansk_before = filtering(before_cloud,"Gda")
+szczecin_before = filtering(before_cloud,"Szczecin")
+'''
+
+capacity_train, capacity_test = train_test_split(capacity, test_size=0.2, random_state=42)
+mixEnergy_train, mixEnergy_test = train_test_split(mixEnergy, test_size=0.2, random_state=42)
+hourMeteo_train, hourMeteo_test = train_test_split(hourMeteo, test_size=0.2, random_state=42)
+before_cloud_train, before_cloud_test = train_test_split(before_cloud, test_size=0.2, random_state=42)
+after_cloud_train, after_cloud_test = train_test_split(after_cloud, test_size=0.2, random_state=42)
+
+
+from sklearn.neural_network import MLPRegressor
+
+# Definicja modelu sieci neuronowej
+model_nn_capacity = MLPRegressor(hidden_layer_sizes=(100, 50), activation='relu', solver='adam', random_state=42)
+model_nn_mixEnergy = MLPRegressor(hidden_layer_sizes=(100, 50), activation='relu', solver='adam', random_state=42)
+model_nn_hourMeteo = MLPRegressor(hidden_layer_sizes=(100, 50), activation='relu', solver='adam', random_state=42)
+model_nn_before_cloud = MLPRegressor(hidden_layer_sizes=(100, 50), activation='relu', solver='adam', random_state=42)
+model_nn_after_cloud = MLPRegressor(hidden_layer_sizes=(100, 50), activation='relu', solver='adam', random_state=42)
+
+# Trenowanie modeli na danych treningowyc
+model_nn_capacity.fit(capacity_train.drop(columns=['Date']), capacity_train['Aggregated Generation Per Type, PSE SA CA, Actual Generation Output, Solar'])
+model_nn_mixEnergy.fit(mixEnergy_train.drop(columns=['Date']), mixEnergy_train['Aggregated Generation Per Type, PSE SA CA, Actual Generation Output, Solar'])
+model_nn_hourMeteo.fit(hourMeteo_train.drop(columns=['Date']), hourMeteo_train['Aggregated Generation Per Type, PSE SA CA, Actual Generation Output, Solar'])
+model_nn_before_cloud.fit(before_cloud_train.drop(columns=['Date']), before_cloud_train['Aggregated Generation Per Type, PSE SA CA, Actual Generation Output, Solar'])
+model_nn_after_cloud.fit(after_cloud_train.drop(columns=['Date']), after_cloud_train['Aggregated Generation Per Type, PSE SA CA, Actual Generation Output, Solar'])
