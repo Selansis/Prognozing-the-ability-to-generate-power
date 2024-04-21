ce713f15f8fc28e8989afb224feb5583fb35487c
Selansis <wojtekmichalik24@gmail.com>
2024-04-21 09:06:18 +0200

    pandas import implemented

--- Prognozing the ability to generate power.py
+++ Prognozing the ability to generate power.py
@@ -1,22 +1,9 @@
 import csv 
-import pandas as pd
-capacity = []
-mixEnergy = []
-hourMeteo = []
+import pandas as pd 
+capacity = pd.read_csv('Energy Poland Installed capacity.csv')
+mixEnergy = pd.read_csv('Mix Energy Poland.csv')
+hourMeteo = pd.read_csv('Godzinowe dane meteo dla poszczególnych lokalizacji.csv')
+before_cloud = pd.read_csv('przed_uwzg_zachmurzenia.csv')
+after_cloud = pd.read_csv('po_uwzg_zachmurzenia.csv')
 
-with open('Energy Poland Installed capacity.csv', 'r') as file:
-  reader = csv.reader(file)
-  for row in reader:
-    capacity.append(row)
-
-
-with open('Mix Energy Poland.csv', 'r') as file:
-  reader = csv.reader(file)
-  for row in reader:
-    mixEnergy.append(row)
-
-with open('Godzinowe dane meteo dla poszczególnych lokalizacji.csv') as file:
-  reader = csv.reader(file)
-  for row in reader:
-    hourMeteo.append(row)
-
+print(capacity)
